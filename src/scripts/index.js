0185396b6ee4f039403afdc5268bfe9ec5aad29e
Danial <d.o.shramkov@gmail.com>
2023-11-01 17:54:22 -0400

    minor cleans

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,5 @@
 import '../styles/style.scss';
 
-// window.onscroll = (e) => {
-//     if(window.scrollY > 100) {
-//         document.querySelector('.header').style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
-//     } else if(window.scrollY < 100) {
-//         document.querySelector('.header').style.backgroundColor = '#fff';
-//     }
-// };
-
 document.addEventListener('DOMContentLoaded', (e) => {
     new Splide( '.splide', {
         type   : 'loop',
@@ -22,13 +14,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
     for (let i = 0; i < accordionButtons.length; i++) {
         accordionButtons[i].addEventListener('click', function(e) {
             e.target.classList.toggle('active');
-            const nodeContent = e.target.nextElementSibling;
-            nodeContent.classList.toggle('active');
-            // if ('block' === nodeContent.style.display) {
-            //     nodeContent.style.display = 'none';
-            // } else {
-            //     nodeContent.style.display = 'block';
-            // }
+            e.target.nextElementSibling.classList.toggle('active');
         });
     }
 
